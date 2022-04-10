26be3b03c43597b597b80e88ac3f11174b5c5bc5
seanhsu17 <seanhsu@17.media>
2022-04-10 23:25:12 +0800

    feat: use channel

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -11,25 +11,42 @@ import "os"
 import "net/rpc"
 import "net/http"
 
+type status int
+
+const (
+	taskReady status = iota
+	taskQueue
+	taskRunning
+	taskDone
+)
+
 type taskStatus struct {
 	NodeID    string
 	StartTime int64
+	Status    status
 }
 
 const (
-	Timeout int64 = 10
+	Timeout int64 = 5
+)
+
+type phase int
+
+const (
+	mapPhase phase = iota
+	reducePhase
 )
 
 type Coordinator struct {
 	// Your definitions here.
-	intermediates    [][]string
-	inputs           []string
-	mapTasks         []int
-	reduceTasks      []int
-	mapDoingTasks    map[int]taskStatus
-	reduceDoingTasks map[int]taskStatus
-	mapDone          bool
-	lock             sync.Mutex
+	intermediates [][]string
+	inputs        []string
+	nReduce       int
+	taskChan      chan int
+	tasks         []taskStatus
+	phase         phase
+	done          bool
+	lock          sync.Mutex
 }
 
 // Your code here -- RPC handlers for the worker to call.
@@ -67,112 +84,106 @@ func (c *Coordinator) server() {
 func (c *Coordinator) Done() bool {
 	c.lock.Lock()
 	defer c.lock.Unlock()
-	return len(c.reduceTasks) == 0 && len(c.reduceDoingTasks) == 0
-}
-
-func (c *Coordinator) checkMapDone() bool {
-	if c.mapDone {
-		return true
-	}
-	c.mapDone = len(c.mapDoingTasks) == 0 && len(c.mapTasks) == 0
-	return c.mapDone
+	return c.done
 }
 
 func (c *Coordinator) GetTask(args *GetTaskArgs, reply *GetTaskReply) error {
-	fmt.Printf("Node %s income\n", args.NodeID)
-	now := time.Now().Unix()
+	task := <-c.taskChan
 	c.lock.Lock()
 	defer c.lock.Unlock()
-	if c.checkMapDone() {
-		if len(c.reduceTasks) == 0 && len(c.reduceDoingTasks) == 0 {
-			reply.TaskType = NoTask
-		} else if len(c.reduceTasks) == 0 {
-			reply.TaskType = WaitTask
-		} else {
-			reply.TaskType = ReduceTask
-			right := len(c.reduceTasks) - 1
-			for len(c.intermediates[c.reduceTasks[right]]) == 0 {
-				right--
-			}
-			task := c.reduceTasks[right]
-			reply.Files = append(reply.Files, c.intermediates[task]...)
-			reply.ID = task
-
-			status := taskStatus{
-				NodeID:    args.NodeID,
-				StartTime: now,
-			}
-			c.reduceDoingTasks[task] = status
-			c.reduceTasks = c.reduceTasks[:right]
-			fmt.Printf("deliver reduce task %d to %s\n", task, args.NodeID)
-		}
+	reply.ID = task
+	reply.ReduceCount = c.nReduce
+	if c.phase == reducePhase {
+		reply.TaskType = ReduceTask
+		reply.Files = append(reply.Files, c.intermediates[task]...)
+		fmt.Printf("deliver reduce task %d to %s\n", task, args.NodeID)
 	} else {
-		if len(c.mapTasks) != 0 {
-			reply.TaskType = MapTask
-			task := c.mapTasks[len(c.mapTasks)-1]
-			reply.Files = append(reply.Files, c.inputs[task])
-			reply.ID = task
-			reply.ReduceCount = len(c.reduceTasks)
-
-			status := taskStatus{
-				NodeID:    args.NodeID,
-				StartTime: now,
-			}
-			c.mapDoingTasks[task] = status
-			c.mapTasks = c.mapTasks[:len(c.mapTasks)-1]
-			fmt.Printf("deliver map task %d %s to %s\n", task, c.inputs[task], args.NodeID)
-		} else {
-			reply.TaskType = WaitTask
-		}
+		reply.TaskType = MapTask
+		reply.Files = append(reply.Files, c.inputs[task])
+		fmt.Printf("deliver map task %d %s to %s\n", task, c.inputs[task], args.NodeID)
 	}
+	c.tasks[task].Status = taskRunning
+	c.tasks[task].NodeID = args.NodeID
+	c.tasks[task].StartTime = time.Now().Unix()
 	return nil
 }
 
 func (c *Coordinator) TaskComplete(args *TaskCompleteArgs, reply *TaskCompleteReply) error {
 	c.lock.Lock()
 	defer c.lock.Unlock()
+
+	if c.tasks[args.ID].NodeID != args.NodeID {
+		fmt.Printf("Task done %d but timeout \n", args.ID)
+		return nil
+	}
+
 	if args.TaskType == MapTask {
-		if _, ok := c.mapDoingTasks[args.ID]; !ok {
-			fmt.Printf("map task done %d but timeout \n", args.ID)
+		if c.phase != mapPhase {
 			return nil
 		}
-		for i, files := range args.Files {
-			c.intermediates[i] = append(c.intermediates[i], files)
+		for i, file := range args.Files {
+			if file == "" {
+				continue
+			}
+			c.intermediates[i] = append(c.intermediates[i], file)
 		}
-		delete(c.mapDoingTasks, args.ID)
 		fmt.Printf("map task done %d \n", args.ID)
 	} else {
-		delete(c.reduceDoingTasks, args.ID)
 		fmt.Printf("reduce task done %d \n", args.ID)
 	}
+	c.tasks[args.ID].Status = taskDone
+	go c.check()
 	return nil
 }
 
-func (c *Coordinator) CheckTimeout() {
-	for {
-		c.lock.Lock()
-		if len(c.mapDoingTasks) == 0 && len(c.mapTasks) == 0 && len(c.reduceTasks) == 0 && len(c.reduceDoingTasks) == 0 {
-			c.lock.Unlock()
-			break
-		}
-		now := time.Now().Unix()
-		for key, value := range c.mapDoingTasks {
-			if now-value.StartTime > Timeout {
-				fmt.Printf("map task timeout %d \n", key)
-				delete(c.mapDoingTasks, key)
-				c.mapTasks = append(c.mapTasks, key)
+func (c *Coordinator) check() {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	if c.done {
+		return
+	}
+	finish := true
+	for i, _ := range c.tasks {
+		switch c.tasks[i].Status {
+		case taskReady:
+			finish = false
+			c.tasks[i].Status = taskQueue
+			c.taskChan <- i
+		case taskQueue:
+			finish = false
+		case taskRunning:
+			finish = false
+			if time.Now().Unix()-c.tasks[i].StartTime > Timeout {
+				c.tasks[i].Status = taskQueue
+				c.taskChan <- i
 			}
 		}
-		for key, value := range c.reduceDoingTasks {
-			if now-value.StartTime > Timeout {
-				fmt.Printf("reduce task timeout %d \n", key)
-				delete(c.reduceDoingTasks, key)
-				c.mapTasks = append(c.reduceTasks, key)
-			}
+	}
+	if finish {
+		if c.phase == mapPhase {
+			c.initReduceTasks()
+		} else {
+			c.done = true
 		}
-		c.lock.Unlock()
+	}
+}
+
+func (c *Coordinator) Check() {
+	for !c.Done() {
+		c.check()
 		time.Sleep(time.Second)
 	}
+}
+
+func (c *Coordinator) initMapTasks() {
+	c.tasks = make([]taskStatus, len(c.inputs))
+	c.phase = mapPhase
+}
+
+func (c *Coordinator) initReduceTasks() {
+	fmt.Println("Start reduce")
+	c.tasks = make([]taskStatus, c.nReduce)
+	c.phase = reducePhase
 }
 
 // MakeCoordinator
@@ -185,20 +196,17 @@ func MakeCoordinator(files []string, nReduce int) *Coordinator {
 
 	// Your code here.
 	c.inputs = files
-	for i := 0; i < len(files); i++ {
-		c.mapTasks = append(c.mapTasks, i)
-	}
-	c.mapDoingTasks = make(map[int]taskStatus, len(files))
-
-	for i := 0; i < nReduce; i++ {
-		c.reduceTasks = append(c.reduceTasks, i)
-	}
-	c.reduceDoingTasks = make(map[int]taskStatus, nReduce)
-	c.intermediates = make([][]string, nReduce)
-
+	c.nReduce = nReduce
 	c.lock = sync.Mutex{}
+	c.intermediates = make([][]string, nReduce)
+	c.initMapTasks()
+	if nReduce > len(files) {
+		c.taskChan = make(chan int, nReduce)
+	} else {
+		c.taskChan = make(chan int, len(c.inputs))
+	}
 
 	c.server()
-	go c.CheckTimeout()
+	go c.Check()
 	return &c
 }
